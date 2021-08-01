b92ec590af2e9c5e5a770289eef5b2075c300d37
Abinandhan17 <53071808+Abinandhan17@users.noreply.github.com>
2021-08-01 20:23:03 +0530

    Add files via upload

---
+++ assignment_day3.py
@@ -0,0 +1,11 @@
+gift_presented_to = [2,1,5,3,4]
+if 2 in gift_presented_to:
+    print("Person P1 has received gift from person P2")
+if 1 in gift_presented_to:
+    print("Person P2 has received gift from person P1")
+if 5 in gift_presented_to:
+    print("Person P4 has received gift from person P5")
+if 3 in gift_presented_to:
+    print("Person P5 has received gift from person P3")
+if 4 in gift_presented_to:
+    print("Person P3 has received gift from person P4")
